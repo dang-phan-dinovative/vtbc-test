c549dc845f49a8d1ca733f7ec3c3cdcf81809fd1
phandang <phdang94@yahoo.com>
2022-12-24 12:34:56 +0700

    auto detect font color base on background image

--- src/components/Vtbc/index.tsx
+++ src/components/Vtbc/index.tsx
@@ -37,9 +37,93 @@ const paramString = Object.keys(params).reduce((str, key) => {
 
 const url = `https://dev-vietinbank.mobilott.vn/${page}?${(paramString)}`
 
+const paintCanvas = (img: HTMLImageElement, bckg: HTMLElement, bckg_measures: DOMRect, text: HTMLElement, text_measures: DOMRect) => {
+          
+  // Creating the canvas element to draw the image
+  let canvas = document.createElement('canvas');
+  let ctx    = canvas.getContext('2d');
+
+  // Setting the size of the canvas
+  canvas.height = bckg_measures.height;
+  canvas.width  = bckg_measures.width;
+
+  // Drawing the final image
+  ctx?.drawImage(img, text.offsetLeft, text.offsetTop, text_measures.width, text_measures.height, 0, 0, text_measures.width, text_measures.height);
+
+  // Getting the image data
+  return ctx?.getImageData(0, 0, text_measures.width, text_measures.height);
+
+}
+
+const getAverageColor = (imgData: ImageData) => {
+    
+  // Calculating average color of canvas
+  // Counting the total elements
+  let rgb   = {r:0, g:0, b: 0};
+  let count = 0;
+
+  length = imgData.data.length;
+
+  for (var i = 0; i < length; i += 4) {
+
+      rgb.r += imgData.data[i];
+      rgb.g += imgData.data[i + 1];
+      rgb.b += imgData.data[i + 2];
+
+      count ++;
+
+ }
+
+  // Calculating average
+  rgb.r = Math.floor(rgb.r / count);
+  rgb.g = Math.floor(rgb.g / count);
+  rgb.b = Math.floor(rgb.b / count);
+
+  // Getting the average of the image
+  return ((rgb.r * 299) + (rgb.g * 587) + (rgb.b * 114)) / 1000;
+
+}
+
+const getModeBackgroundImage = () => {
+  return new Promise ((resolve, reject) => {
+    // 0. Get text and background
+    let text  = document.querySelector('.text > h2') as HTMLElement;
+    let bckg  = document.querySelector('.overlay') as HTMLElement;
+    // 1. Calculate text and background measures
+    let text_measures = text.getBoundingClientRect();
+    let bckg_measures = bckg.getBoundingClientRect();
+    // 2. Cut the background image behind the text
+    let img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src         = window.getComputedStyle(bckg, 'false').backgroundImage.slice(5, -2);
+    img.height      = bckg_measures.height;
+    img.width       = bckg_measures.width;
+
+    img.onload = () => {
+      // 3. Draw the portion of the background on a new canvas
+      let imgData = paintCanvas(img, bckg, bckg_measures, text, text_measures);
+      if (imgData) {
+        // 4. Calculate the YIQ based on the average RGB
+        const fontColor = getAverageColor(imgData) >= 128 ? 'black' : 'white';
+        resolve(fontColor)
+      }
+      reject('Cannot create canvas image')
+    }
+  })
+  
+}
+
+
 const Vtbc = () => {
+  const changeTextColor = async () => {
+    const theme = await getModeBackgroundImage()
+    console.log(theme)
+  }
   useEffect(() => {
+   
     document.title = 'VTBC - Exchange Traded Fund'
+    changeTextColor()
+    
   }, [])
   return (
     <div>
